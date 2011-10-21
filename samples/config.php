81ee5bbb46b7ff7f8ba422b226c5270ee3522831
Stanislau <stanislau.wolski@gmail.com>
2011-10-21 11:20:47 +0300

    [add] SQL Anywhere DB wrapper

--- samples/config.php
+++ samples/config.php
@@ -9,4 +9,6 @@
 	$postrgre_connection = "host=localhost port=5432 dbname=sampleDB user=root password=1234";
 	//necessary for Oracle related samples only 
 	$oci_connection = "some here";
+	//necessart for SQL Anywhere connection
+	$sasql_conn = "uid=DBA;pwd=sql";
 ?>
